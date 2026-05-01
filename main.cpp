d28045a05cf7436e5b984529924061619d2e7174
dmitianna <dmitianna@bk.ru>
2026-05-01 10:51:42 +0700

    Добавлены тестовые сценарии для блоков из кучи

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ void testDefaultNew(size_t N)
 
 // allocator (heap pool)
 // Аллокатор выделяет новые блоки памяти
-void testAllocator(size_t N)
+void testHeapPool(size_t N)
 {
     Allocator alloc(sizeof(MyClass), N);
     MyClass** arr = new MyClass*[N];
@@ -46,7 +46,7 @@ void testAllocator(size_t N)
 
     auto end = Clock::now();
 
-    std::cout << "allocator first try: "<< std::chrono::duration<double, std::milli>(end - start).count()<< " ms\n";
+    std::cout << "allocator(heap pool) first try: "<< std::chrono::duration<double, std::milli>(end - start).count()<< " ms\n";
 
     // освобождение
     // объекты уничтожаются, память возвращается в free-list
@@ -66,7 +66,7 @@ void testAllocator(size_t N)
 
     end = Clock::now();
 
-    std::cout << "allocator reuse from free-list: "<< std::chrono::duration<double, std::milli>(end - start).count()<< " ms\n";
+    std::cout << "allocator(heap pool) reuse from free-list: "<< std::chrono::duration<double, std::milli>(end - start).count()<< " ms\n";
     for (size_t i = 0; i < N; ++i){
         arr[i]->~MyClass();
         alloc.Deallocate(arr[i]);
@@ -74,10 +74,59 @@ void testAllocator(size_t N)
 
     delete[] arr;
 }
+
+void testHeapBlocks(size_t N)
+{
+    Allocator alloc(sizeof(MyClass));
+    MyClass** arr = new MyClass*[N];
+
+    auto start = Clock::now();
+
+    for (size_t i = 0; i < N; ++i)
+    {
+        void* mem = alloc.Allocate(sizeof(MyClass));
+        arr[i] = new (mem) MyClass(i, i * 1.0f);
+    }
+
+    auto end = Clock::now();
+
+    std::cout << "allocator(heap blocks) first try: "<< std::chrono::duration<double, std::milli>(end - start).count()<< " ms\n";
+
+    // освобождение
+    // объекты уничтожаются, память возвращается в free-list
+    for (size_t i = 0; i < N; ++i)
+    {
+        arr[i]->~MyClass();
+        alloc.Deallocate(arr[i]);
+    }
+
+    // reuse
+    // Память берётся из free-list
+    start = Clock::now();
+
+    for (size_t i = 0; i < N; ++i)
+    {
+        void* mem = alloc.Allocate(sizeof(MyClass));
+        arr[i] = new (mem) MyClass(i, i * 2.0f);
+    }
+
+    auto end2 = Clock::now();
+
+    std::cout << "allocator(heap blocks) reuse from free-list: "<< std::chrono::duration<double, std::milli>(end2 - start).count()<< " ms\n";
+
+    for (size_t i = 0; i < N; ++i)
+    {
+        arr[i]->~MyClass();
+        alloc.Deallocate(arr[i]);
+    }
+    delete[] arr;
+}
+
 int main()
 {
     const size_t N = 500000;
     testDefaultNew(N);
-    testAllocator(N);
+    testHeapBlocks(N);
+    testHeapPool(N);
     return 0;
 }
