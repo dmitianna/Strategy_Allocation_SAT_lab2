97e66c7760e2e6dbf1dfd44506a95c13e260b21e
dmitianna <dmitianna@bk.ru>
2026-05-01 10:23:17 +0700

    Добавлены тестовые сценарии для пула из кучи и стандартного выделения через new

--- main.cpp
+++ main.cpp
@@ -1,19 +1,83 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <chrono>
+#include "Allocator.h"
 
-int main(int argc, char *argv[])
+class MyClass
 {
-    QCoreApplication a(argc, argv);
+private:
+    int id;
+    float value;
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
+public:
+    MyClass(int _id, float _value) : id(_id), value(_value) {}
+    ~MyClass() {}
+};
 
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
+using Clock = std::chrono::high_resolution_clock;
 
-    return a.exec();
+//new/delete
+void testDefaultNew(size_t N)
+{
+    auto start = Clock::now();
+    for (size_t i = 0; i < N; ++i)
+    {
+        MyClass* obj = new MyClass(i, i * 1.0f);
+        delete obj;
+    }
+    auto end = Clock::now();
+
+    std::cout << "new/delete: "<< std::chrono::duration<double, std::milli>(end - start).count()<< " ms\n";
+}
+
+// allocator (heap pool)
+// Аллокатор выделяет новые блоки памяти
+void testAllocator(size_t N)
+{
+    Allocator alloc(sizeof(MyClass), N);
+    MyClass** arr = new MyClass*[N];
+
+    // первый проход
+    auto start = Clock::now();
+
+    for (size_t i = 0; i < N; ++i) {
+        void* mem = alloc.Allocate(sizeof(MyClass));
+        arr[i] = new (mem) MyClass(i, i * 1.0f);
+    }
+
+    auto end = Clock::now();
+
+    std::cout << "allocator first try: "<< std::chrono::duration<double, std::milli>(end - start).count()<< " ms\n";
+
+    // освобождение
+    // объекты уничтожаются, память возвращается в free-list
+    for (size_t i = 0; i < N; ++i) {
+        arr[i]->~MyClass();
+        alloc.Deallocate(arr[i]);
+    }
+
+    // reuse
+    // Память берётся из free-list
+    start = Clock::now();
+
+    for (size_t i = 0; i < N; ++i) {
+        void* mem = alloc.Allocate(sizeof(MyClass));
+        arr[i] = new (mem) MyClass(i, i * 2.0f);
+    }
+
+    end = Clock::now();
+
+    std::cout << "allocator reuse from free-list: "<< std::chrono::duration<double, std::milli>(end - start).count()<< " ms\n";
+    for (size_t i = 0; i < N; ++i){
+        arr[i]->~MyClass();
+        alloc.Deallocate(arr[i]);
+    }
+
+    delete[] arr;
+}
+int main()
+{
+    const size_t N = 500000;
+    testDefaultNew(N);
+    testAllocator(N);
+    return 0;
 }
